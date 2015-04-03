d8c3b965711e9172727f99630dbc7f971f580b0d
phpor <phpor.net@gmail.com>
2015-04-03 18:41:47 +0800

    add comment

--- example/use_luaunit.lua
+++ example/use_luaunit.lua
@@ -2,6 +2,7 @@
 -- get luaunit from: https://github.com/bluebird75/luaunit
 local luaunit = require("luaunit")
 
+-- 不管是class还是function，必须不能是local的，local的将不会被测试
 TestClass = {} --class
 function TestClass:test1_method()
 	local a = 1
