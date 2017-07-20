d3f47bd5be139420a37606c1f1d081ebc087ea39
李俊杰 <phpor@users.noreply.github.com>
2017-07-20 18:07:16 +0800

    Update ngx_auth.lua

--- example/ngx_auth.lua
+++ example/ngx_auth.lua
@@ -1,45 +1,81 @@
-
-function auth() 
-	local username = ngx.var.remote_user 
-	local password = ngx.var.remote_passwd 
+local access = {}
+function access.auth()
+	ngx.req.read_body()
+	local args, err = ngx.req.get_post_args()
+	local username
+	local password
+	if args then
+		username = args["portal_username"]
+		password = args["portal_password"]
+	end
 	local sid = ngx.var.cookie_ngx_sid
-	--ngx.say(username)
-	--ngx.say(password)
-	--ngx.exit(200)
 
 	if username and password  then
-		if check_password(username, password) then
-			return username
+		if access:check_password(username, password) then
+			return access:redirect() --Avoid resubmit on refresh
 		end
 	elseif sid then
-		local username = check_session(sid)
+		local username = access:check_session(sid)
 		if username ~= false then
-			return username
+			return access:auth_succ(username)
 		end
 	end
-	show_auth_page()
+	access:show_auth_page()
 end
 
-function show_auth_page()
-	auth_401()
+function access.redirect()
+	ngx.redirect(ngx.var.request_uri, 302)
+	ngx.eof()
+end
+function access.auth_succ(self, username)
+	--ngx.header['x-user'] = username
+	return username
 end
 
-function auth_401()
+function access.show_auth_page(self)
+	ngx.status = 401
+	ngx.header['Content-Type'] = "text/html; charset=utf-8"
+	ngx.say([[
+		<html>
+			<head>
+			<title>login</title>
+			<meta charset="utf-8">
+			<style>
+				.container {text-align: center; margin-top: inherit; }
+			</style>
+			</head>
+			<body>
+			  <section class="container">
+			    <div class="login">
+			      <h1>Login please ...</h1>
+			      <form method="post">
+				<p><input type="text" name="portal_username" value="" placeholder="Username"></p>
+				<p><input type="password" name="portal_password" value="" placeholder="Password"></p>
+				<p class="submit"><input type="submit" name="commit" value="Login"></p>
+			      </form>
+			    </div>
+			  </section>
+			</body>
+		</html>
+	]])
+	ngx.eof()
+end
+
+function access.auth_401(self)
 	ngx.header.www_authenticate = [[Basic realm=""]]
 	ngx.exit(401)
 end
 
-function check_password(username, password)
+function access.check_password(self, username, password)
 	local http = require "resty.http"
 	local cjson = require "cjson"
 	local httpc = http.new()
 	httpc:set_keepalive(0, 100)
 	httpc:set_timeout(time_out)
-	local url = "https://auth.phpor.net/api/ldap/check"
+	local url = "http://phpor.net/api/ldap/check"
 	local params = string.format("username=%s&password=%s&app=nginx&ip=%s", username, password, ngx.var.remote_addr)
 	local res, err = httpc:request_uri(url, {
-		method = "POST",
-		body = params,
+		method = "POST", body = params,
 		headers = {
 			["Content-Type"] = "application/x-www-form-urlencoded",
 		}
@@ -62,41 +98,43 @@ function check_password(username, password)
 		ngx.log(ngx.ERR, "auth fail", url, " ", params ," ", res.body)
 		return false
 	end
-	create_session(username)
+	self:create_session(username)
 	return true
 end
 
-function create_session(username)
-	local sid = make_string(24)
-
-	local redis = get_redis()
-	redis:hset(sid, {username = username})
+function access.create_session(self, username)
+	local sid = self:make_string(24)
+	local redis = self:get_redis()
+	redis:hmset(sid, {username = username})
+	ngx.header['Content-Type'] = "text/html; charset=utf-8"
 	ngx.header['Set-Cookie'] = string.format('ngx_sid=%s; path=/', sid)
 	return sid
 end
 
-function make_string(len)
+function access.make_string(self, len)
         if len < 1 then return nil end -- Check for l < 1
 	local str = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
         local s = "" -- Start string
+	math.randomseed(os.time())
+	math.random() -- the first value maybe not random
         for i = 1, len do
-            n = math.random(1, 62) 
+            n = math.random(1, 62)
 	    s = s .. string.sub(str, n, n)
         end
         return s -- Return string
 end
 
-function system_error()
-	ngx.say("system fail")
-	show_auth_page()
+function access.system_error(self)
+	ngx.say("system error")
+	ngx.exit(500)
 end
-function get_redis() 
-	local redis_info = {
-		host="127.0.0.1",
-		port=6379,
-		--db=0,
-		--password="mypass"
-	}
+function access.get_redis(self)
+        local redis_info = {
+                host="127.0.0.1",
+                port=6379
+                --db=3,
+                --password=""
+        }
 	local redis = require "resty.redis"
 	local red = redis:new()
 	red:set_timeout(3000) -- 3 sec
@@ -110,23 +148,36 @@ function get_redis()
 		local res, err = red:auth(redis_info["password"])
 		if not res then
 			ngx.log("failed to authenticate: ", err)
-			system_error()
+			self:system_error()
 			return false
 		end
 	end
 	return red
 end
 
-function check_session(sid) 
-    local redis = get_redis()
+function access.check_session(self, sid)
+    if #sid ~= 24 then return false end
+    local redis = self:get_redis()
     local res, err = redis:hmget(sid, "username")
     if res and res ~= ngx.null then
     	local username = res[1] or ""
-	if username ~= "" then
+	if username ~= "" and username ~= nil then
 	    	return username
 	end
     end
     return false
 end
 
-auth()
+function access.admin_ip_limit()
+        local ip = ngx.var.remote_addr
+        local pos = string.find(ip, ".", 1, true)
+        local prefix = string.sub(ip, 1, pos - 1)
+        if (prefix == "10" or prefix == "127") then
+                ip = ngx.var.http_x_forwarded_for
+        end
+        if admin_ips:get(ip) == nil then
+                ngx.exit(ngx.HTTP_FORBIDDEN)
+        end
+end
+
+return {auth = access.auth, admin_ip_limit = access.admin_ip_limit}
