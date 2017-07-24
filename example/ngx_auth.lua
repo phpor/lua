8885a1efe5cab52f6893e147b004bea8ea2da13c
root <root@junjie.li-22-37>
2017-07-24 16:07:14 +0800

    添加logout 按钮

--- example/ngx_auth.lua
+++ example/ngx_auth.lua
@@ -1,30 +1,67 @@
 local access = {}
+function access.config_get_login_url(self)
+	return ngx.var.login_url or "/~portal/login"
+end
+function access.config_get_logout_url(self)
+	return ngx.var.logout_url or "/~portal/logout"
+end
+
+function access.config_get_logout_page_url(self)
+	return ngx.var.logout_page_url or "*"
+end
+
+function access.config_get_auth_url(self)
+	return ngx.var.auth_url or "https://phpor.net/api/ldap/check"
+end
+
+function access.config_get_session_ttl(self)
+	return ngx.var.auth_session_ttl or 3600
+end
+
+function access.config_get_redis_info(self)
+        return {
+                host= ngx.var.auth_redis_host or "127.0.0.1",
+                port=ngx.var.auth_redis_port or 6379,
+                db=ngx.var.auth_redis_db or 1,
+                password=ngx.var.auth_redis_password or nil
+        }
+end
 function access.auth()
 	ngx.req.read_body()
 	local args, err = ngx.req.get_post_args()
-	local username
-	local password
+	local username, password, referer
+
 	if args then
 		username = args["portal_username"]
 		password = args["portal_password"]
+		referer = args["portal_referer"] or "/"
 	end
 	local sid = ngx.var.cookie_ngx_sid
 
+	if ngx.var.request_uri == access:config_get_logout_url() then
+		access:logout()
+	end
+
 	if username and password  then
-		if access:check_password(username, password) then
-			return access:redirect() --Avoid resubmit on refresh
+		sid = access:check_password(username, password)
+		if sid then
+			return access:redirect(referer) --Avoid resubmit on refresh
 		end
 	elseif sid then
 		local username = access:check_session(sid)
 		if username ~= false then
+			ngx.log(ngx.ERR, "username: " .. username)
 			return access:auth_succ(username)
 		end
 	end
-	access:show_auth_page()
+	if ngx.var.request_uri == access:config_get_login_url() then
+		return access:show_auth_page()
+	end
+	access:redirect(access:config_get_login_url())
 end
 
-function access.redirect()
-	ngx.redirect(ngx.var.request_uri, 302)
+function access.redirect(self, url)
+	ngx.redirect(url, 302)
 	ngx.eof()
 end
 function access.auth_succ(self, username)
@@ -35,7 +72,7 @@ end
 function access.show_auth_page(self)
 	ngx.status = 401
 	ngx.header['Content-Type'] = "text/html; charset=utf-8"
-	ngx.say([[
+	local str = [[
 		<html>
 			<head>
 			<title>login</title>
@@ -49,15 +86,18 @@ function access.show_auth_page(self)
 			    <div class="login">
 			      <h1>Login please ...</h1>
 			      <form method="post">
-				<p><input type="text" name="portal_username" value="" placeholder="Username"></p>
-				<p><input type="password" name="portal_password" value="" placeholder="Password"></p>
+				<p><input type="text" name="portal_username" value="junjie.li" placeholder="Username"></p>
+				<p><input type="password" name="portal_password" value="Lijunjie1983" placeholder="Password"></p>
+				<p><input type="hidden" name="portal_referer" value="#referer"></p>
 				<p class="submit"><input type="submit" name="commit" value="Login"></p>
 			      </form>
 			    </div>
 			  </section>
 			</body>
 		</html>
-	]])
+	]]
+	str = string.gsub(str, '#referer', ngx.req.referer or "/")
+	ngx.say(str)
 	ngx.eof()
 end
 
@@ -88,7 +128,6 @@ function access.check_password(self, username, password)
 		ngx.log(ngx.ERR, "call " .. url .. " fail ",  err)
 		return false
 	end
-	ngx.log(ngx.INFO, "uri:" .. url .. ", response:" .. res.body)
 	if pcall(cjson.decode, res.body) == false then
 		ngx.log(ngx.ERR, "uri:" .. url .. ",response parse fail:" .. res.body)
 		return false
@@ -98,14 +137,13 @@ function access.check_password(self, username, password)
 		ngx.log(ngx.ERR, "auth fail", url, " ", params ," ", res.body)
 		return false
 	end
-	self:create_session(username)
-	return true
+	return self:create_session(username)
 end
 
 function access.create_session(self, username)
 	local sid = self:make_string(24)
 	local redis = self:get_redis()
-	redis:hmset(sid, {username = username})
+	redis:hmset(sid, 'username' , username)
 	redis:expire(sid, self:config_get_session_ttl())
 	ngx.header['Content-Type'] = "text/html; charset=utf-8"
 	ngx.header['Set-Cookie'] = string.format('ngx_sid=%s; path=/', sid)
@@ -150,34 +188,53 @@ function access.get_redis(self)
 	end
 	return red
 end
-function access.config_get_auth_url(self)
-	return ngx.var.auth_url or "https://sa.beebank.com/api/ldap/check"
-end
-function access.config_get_session_ttl(self)
-	return ngx.var.auth_session_ttl or 3600
-end
-
-function access.config_get_redis_info(self)
-        return {
-                host= ngx.var.auth_redis_host or "127.0.0.1",
-                port=ngx.var.auth_redis_port or 6379,
-                db=ngx.var.auth_redis_db or 1,
-                password=ngx.var.auth_redis_password or nil
-        }
-end
 
 function access.check_session(self, sid)
     if #sid ~= 24 then return false end
     local redis = self:get_redis()
     local res, err = redis:hmget(sid, "username")
-    if res and res ~= ngx.null then
-    	local username = res[1] or ""
-	if username ~= "" and username ~= nil then
-		redis:expire(sid, self:config_get_session_ttl())
-	    	return username
-	end
+    if not res or table.getn(res) == 0 or res[1] == ngx.null then
+	ngx.log(ngx.ERR, 'not res')
+	return false
+    end
+    local username = res[1] or ""
+    
+    if type(username) == "string" and username ~= "" and username ~= nil then
+        redis:expire(sid, self:config_get_session_ttl())
+        return username
     end
     return false
 end
 
-return {auth = access.auth }
+function access.logout(self)
+	local sid = ngx.var.cookie_ngx_sid
+	local redis = self:get_redis()
+	redis:del(sid)
+	ngx.redirect(self:config_get_login_url(), 302)
+	ngx.header['Set-Cookie'] = string.format('ngx_sid=deleted; path=/deleted; expires='..ngx.cookie_time(1), sid)
+	ngx.eof()
+end
+
+function access.append_logout_button()
+	local logout_page_url = access:config_get_logout_page_url()
+	local request_uri = ngx.var.request_uri
+	if logout_page_url ~= "*" then 
+		if logout_page_url ~= request_uri then return end
+	end
+	if request_uri == access:config_get_login_url() or request_uri == access:config_get_logout_url() then return end
+	if not string.gmatch(ngx.header['Content-Type'], "^text/html") then return end
+	local body, eof = ngx.arg[1], ngx.arg[2]
+	if eof then
+		ngx.arg[1] = ngx.arg[1] .. access:logout_html()
+	end
+end
+
+function access.logout_html(self)
+	return [[
+		<div style="float: right;margin: 7px 7px; z-index:9999" class="portal_logout">
+			<a href="/~portal/logout">logout</a>
+		</div>
+	]]
+end
+
+return {auth = access.auth, append_logout_button = access.append_logout_button }
